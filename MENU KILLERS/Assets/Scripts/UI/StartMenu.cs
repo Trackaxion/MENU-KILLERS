ff7fb931e0897ced2f9b1cc4bc6a9e303e56897b
Donovan Peckham <donovanapeckham@hotmail.com>
2026-02-20 18:17:55 -0500

    Start Menu

---
+++ MENU KILLERS/Assets/Scripts/UI/StartMenu.cs
@@ -0,0 +1,75 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StartMenu : MonoBehaviour
+{
+    public GameObject titleMenu;
+    public GameObject settingsMenu;
+    public GameObject controlsMenu;
+    public GameObject audioMenu;
+    public GameObject videoMenu;
+    public void LoadGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void NewGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void Settings()
+    {
+        titleMenu.SetActive(false);
+        settingsMenu.SetActive(true);
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+
+    public void Controls()
+    {
+        settingsMenu.SetActive(false);
+        controlsMenu.SetActive(true);
+    }
+
+    public void Audio()
+    {
+        settingsMenu.SetActive(false);
+        audioMenu.SetActive(true);
+    }
+
+    public void Video()
+    {
+        settingsMenu.SetActive(false);
+        videoMenu.SetActive(true);
+    }
+
+    public void SettingsBack()
+    {
+        settingsMenu.SetActive(false);
+        titleMenu.SetActive(true);
+    }
+
+    public void AudioBack()
+    {
+        audioMenu.SetActive(false);
+        settingsMenu.SetActive(true);
+    }
+
+    public void VideoBack()
+    {
+        videoMenu.SetActive(false);
+        settingsMenu.SetActive(true);
+    }
+
+    public void ControlsBack()
+    {
+        controlsMenu.SetActive(false);
+        settingsMenu.SetActive(true);
+    }
+}
