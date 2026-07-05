575f53ae2a19f0f52894b2dba5fcbfbdceabe2f0
Donovan Peckham <donovanapeckham@hotmail.com>
2026-07-05 13:53:28 -0400

    7/5/2026.2

--- MENU KILLERS/Assets/Scripts/UI/BonusPowerMover.cs
+++ MENU KILLERS/Assets/Scripts/UI/BonusPowerMover.cs
@@ -8,22 +8,23 @@ public class BonusPowerMover : MonoBehaviour
     public float endingSpot;
     public float moveSpeed;
     private Rigidbody2D rb;
+    public GameObject collisionObject;
 
     private void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
     }
 
-    private void Update()
-    {
-        if (transform.position.x <= endingSpot)
-        {
-            transform.position = new Vector2(startingSpot, transform.position.y);
-        }
-    }
-
     private void FixedUpdate()
     {
         transform.Translate(Vector2.left * moveSpeed * Time.deltaTime);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject == collisionObject)
+        {
+            transform.position = new Vector2(startingSpot, transform.position.y);
+        }
     }
 }
